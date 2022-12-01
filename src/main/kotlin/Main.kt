c56b208f1ebb42e275b4a36a27d8386b3f57be25
Jesper Evertsson <jesper@piktiv.se>
2022-12-01 12:52:53 +0100

    Solution for day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    day1(readFile("day01.txt"))
 }
 
 fun readFile(fileName: String): List<String> {
--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -1,4 +1,16 @@
-fun day1() {
-    val lines: List<String> = readFile("day01.txt")
+fun day1(lines: List<String>) {
+    var currentCalories = 0
+    val calorieList = mutableListOf<Int>()
 
+    lines.forEach {
+        if (it.isEmpty()) {
+            calorieList.add(currentCalories)
+            currentCalories = 0
+        } else {
+            currentCalories += Integer.valueOf(it)
+        }
+    }
+
+    calorieList.sortDescending()
+    println(calorieList[0] + calorieList[1] + calorieList[2])
 }
