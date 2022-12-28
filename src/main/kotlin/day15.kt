adc66cc333189d3019ae13450c7521ca9df87c10
Jesper Evertsson <jesper@piktiv.se>
2022-12-28 22:30:22 +0100

    Increase speed of solving day 15

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -7,7 +7,7 @@ fun day15(lines: List<String>) {
     val sensors = findSensors(lines)
     val xRange = findXRange(sensors)
 
-    val answer = findNoBeaconPositions(sensors, xRange)
+    val answer = findNoBeaconPositions(sensors, xRange) - findBeaconsOnRowOfInterest(sensors)
 
     println("Day 15 part 1: $answer")
 
@@ -17,26 +17,120 @@ fun day15(lines: List<String>) {
 }
 
 fun findDistressBeacon(sensors: List<Sensor>): Point {
-    var notPossible = 0
-
     sensors.forEach { sensor ->
-        sensor.perimeterPoints.forEach { point ->
-            run breaking@{
-                notPossible = 0
-                sensors.forEach { innerSensor ->
-                    if (getManhattanDistance(innerSensor.position, point) <= innerSensor.closestBeaconDistance) {
-                        notPossible++
-                        return@breaking
-                    }
-                }
-                if (notPossible == 0) {
-                    return point
-                }
-            }
+        val point = Point(sensor.position.x, sensor.position.y + sensor.closestBeaconDistance + 1)
+        
+        val answer = walkDownRight(sensor, point, sensors) ?: walkDownLeft(sensor, point, sensors) ?: walkUpLeft(sensor, point, sensors) ?: walkUpRight(sensor, point, sensors)
+        
+        if (answer != null) {
+            return answer
         }
     }
 
     return Point(0, 0)
+}
+
+fun walkDownRight(sensor: Sensor, point: Point, sensors: List<Sensor>): Point? {
+    while (true) {
+        point.x++
+        point.y--
+
+        if (point.y == sensor.position.y || pointOutSideSearchArea(point)) {
+            break
+        }
+
+        val answer = findDetectingSensors(sensors, point)
+        if (answer != null) {
+            return answer
+        }
+    }
+    
+    return null
+}
+
+fun walkDownLeft(sensor: Sensor, point: Point, sensors: List<Sensor>): Point? {
+    while (true) {
+        point.x--
+        point.y--
+
+        if (point.x == sensor.position.x || pointOutSideSearchArea(point)) {
+            break
+        }
+
+        val answer = findDetectingSensors(sensors, point)
+        if (answer != null) {
+            return answer
+        }
+    }
+    
+    return null
+}
+
+fun walkUpLeft(sensor: Sensor, point: Point, sensors: List<Sensor>): Point? {
+    while (true) {
+        point.x--
+        point.y++
+
+        if (point.y == sensor.position.y || pointOutSideSearchArea(point)) {
+            break
+        }
+
+        val answer = findDetectingSensors(sensors, point)
+        if (answer != null) {
+            return answer
+        }
+    }
+    
+    return null
+}
+
+fun walkUpRight(sensor: Sensor, point: Point, sensors: List<Sensor>): Point? {
+    while (true) {
+        point.x++
+        point.y++
+
+        if (point.x == sensor.position.x || pointOutSideSearchArea(point)) {
+            break
+        }
+
+        val answer = findDetectingSensors(sensors, point)
+        if (answer != null) {
+            return answer
+        }
+    }
+    
+    return null
+}
+
+fun findDetectingSensors(sensors: List<Sensor>, point: Point): Point? {
+    var notPossible = 0
+    
+    run breaking@{
+        notPossible = 0
+        sensors.forEach { innerSensor ->
+            if (getManhattanDistance(innerSensor.position, point) <= innerSensor.closestBeaconDistance) {
+                notPossible++
+                return@breaking
+            }
+        }
+        if (notPossible == 0) {
+            return point
+        }
+    }
+    
+    return null
+}
+
+fun pointOutSideSearchArea(point: Point): Boolean {
+    return point.x < 0 || point.y < 0 || point.x > MAX_VALUE || point.y > MAX_VALUE
+}
+
+fun findBeaconsOnRowOfInterest(sensors: List<Sensor>): Int {
+    return sensors
+        .map { it.closestBeacon }
+        .filter { it.y == Y_ROW }
+        .toSet()
+        .size
 }
 
 fun findNoBeaconPositions(sensors: List<Sensor>, xRange: LongRange): Int {
@@ -59,50 +153,6 @@ fun findNoBeaconPositions(sensors: List<Sensor>, xRange: LongRange): Int {
     return noBeaconPositions
 }
 
-fun computePerimeterPoints(sensor: Sensor) {
-    val points = mutableListOf<Point>()
-    var point = Point(sensor.position.x, sensor.position.y + sensor.closestBeaconDistance + 1)
-
-    points.add(point)
-    while (true) { // Walk down right
-        point = Point(point.x + 1, point.y - 1)
-        points.add(point)
-
-        if (point.y == sensor.position.y) {
-            break
-        }
-    }
-
-    while (true) { // Walk down left
-        point = Point(point.x - 1, point.y - 1)
-        points.add(point)
-
-        if (point.x == sensor.position.x) {
-            break
-        }
-    }
-
-    while (true) { // Walk up left
-        point = Point(point.x - 1, point.y + 1)
-        points.add(point)
-
-        if (point.y == sensor.position.y) {
-            break
-        }
-    }
-
-    while (true) { // Walk up right
-        point = Point(point.x + 1, point.y + 1)
-        points.add(point)
-
-        if (point.x == sensor.position.x) {
-            break
-        }
-    }
-
-    sensor.perimeterPoints.addAll(points.filter { it.x in 0..MAX_VALUE && it.y in 0..MAX_VALUE })
-}
-
 fun findSensors(lines: List<String>): List<Sensor> {
     val sensors = mutableListOf<Sensor>()
     lines.forEach {
@@ -110,8 +160,7 @@ fun findSensors(lines: List<String>): List<Sensor> {
         val sensorPos = Point(line.split(":")[0].split(",")[0].toLong(), line.split(":")[0].split(",")[1].toLong())
         val beaconPos = Point(line.split(":")[1].split(",")[0].toLong(), line.split(":")[1].split(",")[1].toLong())
         
-        val sensor = Sensor(sensorPos, beaconPos, getManhattanDistance(sensorPos, beaconPos), mutableListOf())
-        computePerimeterPoints(sensor)
+        val sensor = Sensor(sensorPos, beaconPos, getManhattanDistance(sensorPos, beaconPos))
         sensors.add(sensor)
     }
 
@@ -119,18 +168,17 @@ fun findSensors(lines: List<String>): List<Sensor> {
 }
 
 fun findXRange(sensors: List<Sensor>): LongRange {
-    return sensors.minOf { it.closestBeacon.x } - 1500000L..sensors.maxOf { it.closestBeacon.x } + 1500000L
+    return sensors.minOf { it.closestBeacon.x } - 1500000L..sensors.maxOf { it.closestBeacon.x } + 500000L
 }
 
 fun getManhattanDistance(left: Point, right: Point): Long {
     return abs(left.x - right.x) + abs(left.y - right.y)
 }
 
-data class Point(val x: Long, val y: Long)
+data class Point(var x: Long, var y: Long)
 
 data class Sensor(
     val position: Point,
     val closestBeacon: Point,
-    val closestBeaconDistance: Long,
-    val perimeterPoints: MutableList<Point>
+    val closestBeaconDistance: Long
 )
